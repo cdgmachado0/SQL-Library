8ff9ccd6b559fea4168ea6f68f6fe2043f5b574f
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-02-23 19:44:36 +0100

    With the data not overriden

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const sequelize = require('./models').sequelize;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -37,5 +38,15 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// connection to database
+// (async () => {
+//   try {
+//     await sequelize.sync();
+//     console.log('Connection to database successfull!');
+//   } catch (error) {
+//     console.error('Error connecting to database: ', error);
+//   }
+// })();
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const Book = require('../models/book.js');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const books = await Book.findAll();
+  console.log('hi');
+  console.log(books.toJSON());
+  res.json(books);
 });
 
 module.exports = router;
