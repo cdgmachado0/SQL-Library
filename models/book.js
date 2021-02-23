bb827241c58930efdc069bc478c6ff24f300a798
cdgmachado0 <59457858+cdgmachado0@users.noreply.github.com>
2021-02-23 01:14:11 +0100

    On number 5: Test connection...

---
+++ models/book.js
@@ -0,0 +1,39 @@
+'use strict';
+const { Sequelize } = require('sequelize');
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Book extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Book.init({
+    title: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        msg: 'Title cannot be empty'
+      }
+    },
+    author: {
+      type: Sequelize.STRING,
+      allowNull: false,
+      validate: {
+        msg: 'Author cannot be empty'
+      }
+    },
+    genre: DataTypes.STRING,
+    year: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Book',
+  });
+  return Book;
+};
